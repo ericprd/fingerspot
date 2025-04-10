639d9d2aef52a4ae758674ab4132fe91723612aa
0xCA7 <ericprd_@outlook.com>
2025-04-10 18:27:49 +0700

    init: new project

---
+++ RedLight/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace RedLight
+{
+    partial class Window
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.fingerBox = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.fingerBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(116, 721);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(330, 70);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Scan Finger";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // fingerBox
+            // 
+            this.fingerBox.Location = new System.Drawing.Point(116, 54);
+            this.fingerBox.Name = "fingerBox";
+            this.fingerBox.Size = new System.Drawing.Size(330, 459);
+            this.fingerBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.fingerBox.TabIndex = 1;
+            this.fingerBox.TabStop = false;
+            this.fingerBox.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(116, 544);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(330, 83);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "Username:";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(28, 28);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(566, 848);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.fingerBox);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Window";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FingerSpot";
+            this.Load += new System.EventHandler(this.Window_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.fingerBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox fingerBox;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ RedLight/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Windows.Forms;
+using FlexCodeSDK;
+using Npgsql;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+
+namespace RedLight
+{
+    public partial class Window : Form
+    {
+        private const string HOST = "localhost";
+        private const string USERNAME = "postgres";
+        private const string PASSWORD = "admin";
+        private const string PORT = "5432";
+        private const string DB = "fp";
+
+        private readonly string connStr = $"Host={HOST};Port={PORT};Username={USERNAME};Password={PASSWORD};Database={DB}";
+
+        private const string SN = "SE20J04713";
+        private const string VC = "15605307E5667BD";
+        private const string AC = "KH1F61E963F820F9D59910WS";
+        private const string PC = "helloword";
+
+
+        FlexCodeSDK.FinFPVer ver;
+        string empid = "";
+
+        Npgsql.NpgsqlConnection conn = null;
+        public Window()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string query = "SELECT u.name, f.finger_print from users u left join fingers f on f.user_id = u.id where f.finger_print is not null";
+
+            using (var cmd = new NpgsqlCommand(query, conn))
+            using (var  reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader.GetString(0));
+                    ver.FPLoad(reader.GetString(0), 0, reader.GetString(1), PC + reader.GetString(0));
+                }
+            }
+
+            ver.FPVerificationStart();
+        }
+
+        private void Window_Load(object sender, EventArgs e)
+        {
+            ver = new FlexCodeSDK.FinFPVer();
+            ver.FPVerificationID += new __FinFPVer_FPVerificationIDEventHandler(ver_FPVerificationID);
+            ver.FPVerificationImage += new __FinFPVer_FPVerificationImageEventHandler(ver_FPVerificationImage);
+            ver.FPVerificationStatus += new __FinFPVer_FPVerificationStatusEventHandler(ver_FPVerificationStatus);
+
+            ver.AddDeviceInfo(SN, VC, AC);
+
+            ver.PictureSampleHeight = (short)(fingerBox.Height * 15); //FlexCodeSDK use Twips. 1 pixel = 15 twips
+            ver.PictureSampleWidth = (short)(fingerBox.Width * 15); //FlexCodeSDK use Twips. 1 pixel = 15 twips
+
+            ver.PictureSamplePath = AppDomain.CurrentDomain.BaseDirectory + "Finger.bmp";
+
+            conn = new NpgsqlConnection(connStr);
+            conn.Open();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        void ver_FPVerificationID(string ID, FingerNumber FingerNr)
+        {
+            empid = ID;
+        }
+
+        void ver_FPVerificationImage()
+        {
+            fingerBox.Load(AppDomain.CurrentDomain.BaseDirectory + "Finger.bmp");
+        }
+
+        void ver_FPVerificationStatus(VerificationStatus Status)
+        {
+            if (Status == VerificationStatus.v_OK)
+            {
+                textBox1.Text = textBox1.Text + "\r\n" + "ID : " + empid;
+            }
+            else if (Status == VerificationStatus.v_NotMatch)
+            {
+                textBox1.Text = textBox1.Text + "\r\n" + "Not recognized";
+            }
+            else if (Status == VerificationStatus.v_NoDevice)
+            {
+                textBox1.Text = textBox1.Text + "\r\n" + "DEVICE_NOT_DETECTED";
+            }
+            
+            ver.FPVerificationStop();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
